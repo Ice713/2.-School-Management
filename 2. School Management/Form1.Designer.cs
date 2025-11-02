83afca366d3b403626bbf4ef1126c833c5adedfe
Ice713 <icramzilruiz@gmail.com>
2025-11-03 02:13:47 +0800

    Working on UI

--- 2. School Management/Form1.Designer.cs
+++ 2. School Management/Form1.Designer.cs
@@ -28,13 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.numericUpDownGrade = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonDisplayStudent = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownGrade)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(236, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(333, 131);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(120, 22);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // numericUpDownGrade
+            // 
+            this.numericUpDownGrade.Location = new System.Drawing.Point(333, 159);
+            this.numericUpDownGrade.Name = "numericUpDownGrade";
+            this.numericUpDownGrade.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDownGrade.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(236, 161);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Grade Level:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(56, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(714, 56);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "School Management System";
+            // 
+            // buttonDisplayStudent
+            // 
+            this.buttonDisplayStudent.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.buttonDisplayStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDisplayStudent.ForeColor = System.Drawing.SystemColors.Control;
+            this.buttonDisplayStudent.Location = new System.Drawing.Point(276, 205);
+            this.buttonDisplayStudent.Name = "buttonDisplayStudent";
+            this.buttonDisplayStudent.Size = new System.Drawing.Size(146, 78);
+            this.buttonDisplayStudent.TabIndex = 5;
+            this.buttonDisplayStudent.Text = "Display Student Info";
+            this.buttonDisplayStudent.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.buttonDisplayStudent);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDownGrade);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "2. School Management";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownGrade)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.NumericUpDown numericUpDownGrade;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonDisplayStudent;
     }
 }
 
